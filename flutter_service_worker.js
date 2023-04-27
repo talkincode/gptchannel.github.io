23c3b5329b10c5aa8fb6006565ce7e20e6655028
wangjuntao <jamiesun.net@gmail.com>
2023-04-28 00:22:11 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "02169e5b6ec53f0538109215f2953e80",
-"index.html": "07e0298ca57c972df002b13515a64412",
-"/": "07e0298ca57c972df002b13515a64412",
-"main.dart.js": "75a6f8603251954e8933b6e4feb79fa3",
+"index.html": "d6e27c8c7559c992c44dd8cce0167fc3",
+"/": "d6e27c8c7559c992c44dd8cce0167fc3",
+"main.dart.js": "34523a0377b56b6b7ea904de8d34de4a",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "c3118cd6d2363dc618bdb08abad9176f",
 "icons/Icon-192.png": "3dd627744c922cb1e450618075cf3406",
